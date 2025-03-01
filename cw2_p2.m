b3e8b9648f632f79b68aa17b4c863d998a15f65e
Michael Mitchell <michaelmitch2002@gmail.com>
2025-03-01 09:27:07 +0000

    Sparse Structure, faster

--- cw2_p2.m
+++ cw2_p2.m
@@ -48,59 +48,58 @@ function [U, iterhis] = sor(iterative, A, b, U, N, err, omega)
     [U, iterhis] = iterative(A, M, Never, b, U, N, err);
 end
 
-%% Grid
-
-function [x2, y2, A, b] = gridcreate(N, M)
+%% Grid Creation
+function [x2, y2, A, b, x, y, U] = gridcreate(N, M)
+    % Define Boundary Conditions
     x_0 = 0; x_N = 5; y_0 = 0; y_N = 3;
     U_0 = 0; dU_1 = 0; U_2 = 1;
 
+    % Create Plotting Mesh
+    x = linspace(x_0, x_N, N); y = linspace(y_0, y_N, M);
+    [x, y] = meshgrid(x, y);
+    U = zeros(size(x));
+
+    % Create Solution Points
     x2 = linspace(x_0, x_N, N); y2 = linspace(y_0, y_N, M);
     [x2, y2] = meshgrid(x2, y2);
+    x2 = reshape(x2(1:end-1, 2:end-1)', (N-2)*(M-1), 1); y2 = reshape(y2(1:end-1, 2:end-1)', (N-2)*(M-1), 1);
 
-    x2 = reshape(x2(2:end-1, 2:end-1)', (N-2)*(M-2), 1); y2 = reshape(y2(2:end-1, 2:end-1)', (N-2)*(M-2), 1);
-
+    % Adaptive Grid
     omega = ((6.5 - y2) - x2)*(N - 1)/5;
     omega(omega > 1) = 1;
     theta = ((6.5 - x2) - y2)*(M - 1)/3;
     theta(theta > 1) = 1;
 
-    ind2 = find(x2 + y2 < (6.5) == 1);
+    % Reduce Solution Points to locations needed solving
+    ind2 = find((x2 + y2 < (6.5)) + (y2 > 0) == 2);
+    d11 = x2 > 3.5;
+    d12 = x2 < 4.5;
+    d13 = y2 == 0;
+    ind3 = find((d11 == 1) + (d12 == 1) + (d13 == 1) == 3);
+    ind2 = union(ind2, ind3);
     x2 = x2(ind2); y2 = y2(ind2);
-    d11 = x2(1:N-2) > 3.5
-    d12 = x2(1:N-2) < 4.5
-    test = find(d11 == d12)
-    add = sum(d11 == d12)
-    
-    % Construct Matrix
+
+    % Define "x" part of A
     Fx = (spdiags([ -1 2 -1 ], -1:1, (N - 2), (N - 2)));
-    C = kron(speye(M - 2), Fx);
+    C = kron(speye(M - 1), Fx);
     C = spdiags(spdiags(C, -1:1).*[2./(1 + omega), 1./omega, 2./(1 + omega)], -1:1, C)';
 
-    Fy = (spdiags([ -1 2 -1 ], -1:1, (M - 2), (M - 2)));
-    B = kron(Fy, speye(N-2));
+    % Define "y" part of A
+    Fy = (spdiags([ -1 2 -1 ], -1:1, (M - 1), (M - 1)));
+    B = kron(Fy, speye(N - 2));
     B = spdiags(spdiags(B, [-(N-2) 0 (N-2)]).*[2./(1 + theta), 1./theta, 2./(1 + theta)], [-(N-2) 0 (N-2)], B)';
+    g = spdiags(B, [-(N-2) 0 (N-2)]);
+    g(N - 2: 2*(N - 2) , 3) = -2;
+    B = spdiags(g, [-(N-2) 0 (N-2)], B);
 
+    % Reduce components to locations needed solving and combine
     C = C(ind2, ind2); B = B(ind2, ind2);
     A = C + B;
 
-    A = full(A);
-    A2 = zeros(size(A) + add);
-    A2(1:size(A), 1:size(A)) = A;
-    A2(size(A)+1:end, size(A)+1:end) = 4*eye(add) + diag(-1*ones(add - 1, 1), 1) + diag(-1*ones(add - 1, 1), -1);
-    A2(end-add + 1:end, test) = -2*eye(add);
-    A2(test, end-add + 1:end) = -eye(add);
-
-    A = A2;
-
-    xtest = x2(1:N-2);
-    xtest = xtest(d11 == d12);
-
-    x2 = [x2; xtest];
-    y2 = [y2; zeros(size(xtest))];
-
+    %BCs
     b = zeros(length(A), 1);
-    b(end - add + 1) = 1;
-    for k = 1:(N-2)
+    b(1) = 1;
+    for k = length(ind3): length(ind3) + (N-3)
         if x2(k) <= 0.5 || x2(k) >= 4.5
             b(k) = 0;
         elseif x2(k) <= 1.5
@@ -113,13 +112,39 @@ function [x2, y2, A, b] = gridcreate(N, M)
     end
 end
 
-% Construct Grid
-N = 101; M = 61;
-   
-[x2, y2, A, b] = gridcreate(N, M);
-sol = A\b
+% Mesh Pts.
+N = 201; M = 121;
+
+% Gaussian
+[x2, y2, A, b, x, y, U] = gridcreate(N, M);
+sol = A\b;
+
+% Plotting
+n = 0;
+for i = 1:M
+    for j = 1:N
+        indx = find(x2 == x(i, j));
+        indy = find(y2 == y(i, j));
+        ind = intersect(indx, indy);
+        if isempty(ind) == 1
+            if y(i, j) == 0 && x(i,j) > 0.5 && x(i,j) < 1.5
+                U(i, j) = x(i, j) - 0.5;
+            elseif y(i, j) == 0 && x(i,j) >= 1.5 && x(i,j) <= 3.5
+                U(i, j) = 1;
+            elseif y(i,j) + x(i,j) > 6.5
+                U(i, j) = NaN;
+            else
+                U(i,j) = 0;
+            end
+        else
+            U(i,j) = sol(ind);
+        end
+    end
+end
+
+figure(2)
+contourf(x, y, U, 20)
+
 figure(1)
-plot3(x2, y2, sol, '*')
-hold on
-plot3(linspace(0,5,N), linspace(0,0,N), [0; b(1:70); sol(end - 18: end); b(90:N-2); 0],  '*')
+plot3(x, y, U, '*')
 
